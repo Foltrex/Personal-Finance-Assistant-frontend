0841c79524c3e037f42088ba393c11ae64dd08c9
acharniankou <acharniankou@scnsoft.com>
2026-05-04 15:59:05 +0300

    feat(auth): add login and registration form

---
+++ src/pages/auth/signup/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-container fill-height fluid>
+        <v-row justify="center" align="center">
+            <v-card class="w-25 ml-50">
+                <v-card-title>
+                    Sign Up
+                </v-card-title>
+                <v-card-text>
+                    <v-text-field label="Username"></v-text-field>
+                    <v-text-field label="Password"></v-text-field>
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn>
+                        Register
+                    </v-btn>
+                </v-card-actions>
+                <v-card-text>
+                    Already have an account? <router-link to="/signin">Sign In</router-link>
+                </v-card-text>
+            </v-card>
+        </v-row>
+    </v-container>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -8,9 +8,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Index from "@/pages/index.vue";
 import AddExpenseForm from "@/components/AddExpenseForm.vue";
+import Signin from "@/pages/auth/signin/index.vue";
+import Signup from "@/pages/auth/signup/index.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_BASE_URL),
   routes: [
     {
       path: "/",
@@ -20,7 +22,14 @@ const router = createRouter({
       path: "/add-expense",
       component: AddExpenseForm
     },
-    { path: '/test', component: { template: '<h1>Test</h1>' } }
+    {
+      path: "/signin",
+      component: Signin
+    },
+    {
+      path: "/signup",
+      component: Signup
+    }
   ],
 });
 
